139f06fdcc1f3beaec772b8bf6dd290d464ad631
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-08-22 07:36:13 -0500

    finished one test and installation of jest

--- strangers-things/src/components/tests.js
+++ strangers-things/src/components/tests.js
@@ -5,22 +5,16 @@ import App from './App'
 
 describe('Logged in or logged out tests', () => {
     test('should return logged out status initially', () => {
-      const response = await request(app).get('/api/auth/status');
+      const response = await request(App).get('/api/auth/status');
       expect(response.status).toBe(200);
       expect(response.body.loggedIn).toBe(false);
     });
   
     test('should return logged in status after successful login', async () => {
-      const response = await request(app)
+      const response = await request(App)
         .post('/api/auth/login')
         .send({ username: 'testuser', password: 'password123' });
       expect(response.status).toBe(200);
       expect(response.body.loggedIn).toBe(true);
-    });
-  
-    test('should return logged out status after logout', async () => {
-      const response = await request(app).post('/api/auth/logout');
-      expect(response.status).toBe(200);
-      expect(response.body.loggedIn).toBe(false);
     });
   });
